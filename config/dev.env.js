ff6fec9dc9a31d4111f4d5546f5c00c431c2980d
zpjune <1084013577@qq.com>
2018-09-11 15:29:59 +0800

    修改配置

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,8 +5,8 @@ module.exports = {
   // BASE_API: '"http://192.168.1.122:51332"'
   // BASE_API: '"http://192.168.1.116:9090/"'
   // BASE_API: '"http://10.201.1.82:12345/"'
-    BASE_API: '"http://localhost:9090/"',
-    BASE_HOME_API:'"http://192.168.1.107:2121/"'
+    BASE_API: '"http://localhost:5000/"',//自己后端api接口地址
+    BASE_HOME_API:'"http://192.168.1.107:2121/"'//前台后端api接口地址
   //BASE_API: '"http://172.27.253.1:9090/"'
   // BASE_API: '"http://192.168.1.127:51332"'
 }
