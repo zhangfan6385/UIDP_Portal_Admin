12e5c39f8dbe0aaa1e67a9dba1d0019477c98a66
YZ <532050072@qq.com>
2018-11-08 09:47:21 +0800

    1.修改登录页

--- src/frame_src/views/login/index.vue
+++ src/frame_src/views/login/index.vue
@@ -1,67 +1,90 @@
 <template>
-  <div class="login-container">
-    <el-form class="login-form" autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left">
-      <div class="logo title">
-        <img src="../../../frame_src/imgs/logo.png">
-        <!-- <img src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif"> -->
-        <span>{{sysmessage}}</span>
-      </div>
-      <el-form-item prop="username">
-        <span class="svg-container svg-container_login">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="账号" />
+    <div id="login-container" class="login-container">
+        <el-container>
+            <el-header>
+                <el-row type="flex">
+                    <el-col :span="2"></el-col>
+                    <el-col :span="10">
+                        <div class="title">
+                            <img src="../../../frame_src/imgs/logo.png">
+                            {{sysmessage}}
+                        </div>
+                    </el-col>
+                    <el-col :span="10">
+                        <div class="info">
+                            {{currentTime}}
+                        </div>
+                    </el-col>
+                </el-row>
+            </el-header>
+            <div style="width:100%;height:100%;" class="back">
+                <el-row type="flex">
+                    <el-col :span="2"></el-col>
+                    <el-col :span="20">
+                        <el-main>
+                            <div class="loginform">
+                                <div class="header">
+                                    <div class="logo">
+                                        用户登录
+                                    </div>
+                                </div>
+                                <div>
+                                    <el-form class="login-form" autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left">
+                                        <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="请输入账号">
+                                            <template slot="prepend">
+                                                <svg-icon icon-class="user" />
+                                            </template>
+                                            <el-dropdown class="show-pwd" @command="handleCommand" slot="append">
+                                                <span class="el-dropdown-link">
+                                                    <span style="font-size:8px">{{radio}}</span>
+                                                    <i class="el-icon-arrow-down el-icon--right"></i>
+                                                </span>
+                                                <el-dropdown-menu slot="dropdown">
+                                                    <div v-for="(item,index) in list" :key="index">
+                                                        <el-dropdown-item :command="item.key"><span style="font-size:10px">{{item.key}}</span></el-dropdown-item>
+                                                    </div>
+                                                </el-dropdown-menu>
+                                            </el-dropdown>
+                                        </el-input>
+                                        <div class="verification">
+                                            <span v-if="verification1===1">账号不能为空！</span>
+                                        </div>
 
-        <el-dropdown class="show-pwd" @command="handleCommand">
-          <span class="el-dropdown-link">
-            {{radio}}<i class="el-icon-arrow-down el-icon--right"></i>
-          </span>
-          <el-dropdown-menu slot="dropdown">
-            <div v-for="item in list" :key="item.key">
-              <el-dropdown-item :command="item.key">{{item.key}}</el-dropdown-item>
+                                        <el-input name="password" :type="passwordType" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on" placeholder="密码">
+                                            <template slot="prepend">
+                                                <svg-icon icon-class="password" />
+                                            </template>
+                                            <template slot="append">
+                                                <el-button icon="el-icon-view" @click="showPwd" />
+                                            </template>
+                                        </el-input>
+                                        <div class="verification">
+                                            <span v-if="verification2===1">密码不能为空！</span>
+                                            <span v-if="verification2===2">密码必须大于6位！</span>
+                                        </div>
+
+                                        <el-button type="primary" class="button" :loading="loading" @click.native.prevent="handleLogin">用户登录</el-button>
+
+                                    </el-form>
+                                </div>
+                            </div>
+
+                            <el-dialog width="40%" :title="$t('login.loginToUserCode')" :visible.sync="showDialog" append-to-body>
+                                <log-in-user :updateShowDialog='updateShowDialog' />
+                            </el-dialog>
+                        </el-main>
+                    </el-col>
+                    <!-- <el-col :span="2"></el-col> -->
+                </el-row>
             </div>
-          </el-dropdown-menu>
-        </el-dropdown>
 
-      </el-form-item>
-
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input name="password" :type="passwordType" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on" placeholder="密码" />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon icon-class="eye" />
-        </span>
-      </el-form-item>
-      <!--<div>
-         <el-radio v-model="radio" label="ptrUser">ptr账号</el-radio> 
-        <el-radio v-model="radio" label="user">员工账号</el-radio>
-        <el-radio v-model="radio" label="localUser">本地账号</el-radio>
-       </div> -->
-      <br>
-      <el-button type="primary" style="width:100%;margin-bottom:30px;background-color:#409EFF;border-color:#409EFF" :loading="loading" @click.native.prevent="handleLogin">{{$t('login.logIn')}}</el-button>
-      <div class="copyright">
-        ©{{copyright}}&nbsp;&nbsp;版权所有
-      </div>
-
-      <!-- <div class="tips">
-        <span>{{$t('login.username')}} : admin</span>
-        <span>{{$t('login.password')}} : {{$t('login.any')}}</span>
-      </div> -->
-      <!-- <div class="tips">
-        <span style="margin-right:18px;">{{$t('login.username')}} : editor</span>
-        <span>{{$t('login.password')}} : {{$t('login.any')}}</span>
-      </div> -->
-
-      <!-- <el-button class="thirdparty-button" type="primary" @click="showDialog=true">{{$t('login.thirdparty')}}</el-button>  -->
-    </el-form>
-
-    <el-dialog width="40%" :title="$t('login.loginToUserCode')" :visible.sync="showDialog" append-to-body>
-      <log-in-user :updateShowDialog='updateShowDialog' />
-    </el-dialog>
-
-  </div>
+            <el-footer>
+                <div class="copyright">
+                    ©{{copyright}}&nbsp;版权所有
+                </div>
+            </el-footer>
+        </el-container>
+    </div>
 </template>
 
 <script>
@@ -122,7 +145,9 @@ export default {
             sysmessage: "测试平台",
             copyright: "",
             CONF_CODE:
-                "'PTR_IDENT','LOCAL_IDENT','SYS_NAME','CopyRight','CLOUD_ORG'"
+                "'PTR_IDENT','LOCAL_IDENT','SYS_NAME','CopyRight','CLOUD_ORG'",
+            verification1: 0,
+            verification2: 0
         };
     },
     methods: {
@@ -144,8 +169,20 @@ export default {
         },
         handleLogin() {
             this.$store.state.user.token = "";
-            this.$refs.loginForm.validate(valid => {
-                if (valid) {
+            if (
+                this.loginForm.username === null ||
+                this.loginForm.username === ""
+            ) {
+                this.verification1 = 1;
+            } else {
+                if (
+                    this.loginForm.password === null ||
+                    this.loginForm.password === ""
+                ) {
+                    this.verification2 = 1;
+                } else if (this.loginForm.password.length < 6) {
+                    this.verification2 = 2;
+                } else {
                     this.loading = true;
                     this.loginForm.userDomain = this.code;
                     //console.log(this.loginForm); // this.radio // 后改需求走登陆账号 所以没有 域账号的说法了
@@ -179,11 +216,48 @@ export default {
                             this.loading = false;
                             Message.error(err);
                         });
-                } else {
-                    //console.log("error submit!!");
-                    return false;
                 }
-            });
+            }
+            // this.$refs.loginForm.validate(valid => {
+            //     if (valid) {
+            //         this.loading = true;
+            //         this.loginForm.userDomain = this.code;
+            //         //console.log(this.loginForm); // this.radio // 后改需求走登陆账号 所以没有 域账号的说法了
+            //         this.$store
+            //             .dispatch("LoginByUsername", this.loginForm)
+            //             .then(response => {
+            //                 this.$store.dispatch(
+            //                     "setRoleLevel",
+            //                     response.data.roleLevel
+            //                 );
+            //                 if (response.data.roleLevel === "admin") {
+            //                     this.$store.dispatch("setUserId", null);
+            //                     this.updateShowDialog("");
+            //                 } else {
+            //                     var userList = this.$store.state.user.userList;
+            //                     if (userList.length === 1) {
+            //                         // this.$store.dispatch('setDepartCode', orglist[0].orgId)
+            //                         // this.$store.dispatch('setDepartName', orglist[0].orgName)
+            //                         this.$store.dispatch(
+            //                             "setUserId",
+            //                             userList[0].USER_ID
+            //                         );
+            //                         this.updateShowDialog("");
+            //                     } else {
+            //                         this.showDialog = true;
+            //                     }
+            //                     this.loading = false;
+            //                 } // this.$router.push({ path: '/' })
+            //             })
+            //             .catch(err => {
+            //                 this.loading = false;
+            //                 Message.error(err);
+            //             });
+            //     } else {
+            //         //console.log("error submit!!");
+            //         return false;
+            //     }
+            // });
         },
         afterQRScan() {
             // const hash = window.location.hash.slice(1)
@@ -230,7 +304,7 @@ export default {
 
     mounted() {
         this.GetTitle();
-        this.GetColor(); 
+        this.GetColor();
     },
     created() {
         // window.addEventListener('hashchange', this.afterQRScan)
@@ -241,131 +315,137 @@ export default {
 };
 </script>
 
-<style rel="stylesheet/scss" lang="scss">
-// $bg:#2d3a4b;
-$bg: rgb(22, 86, 155);
-$light_gray: #eee;
 
-/* reset element-ui css */
+<style lang="scss">
 .login-container {
-    padding: 50px 0;
-    .el-input {
-        display: inline-block;
-        height: 47px;
-        width: 70%;
-        input {
-            background: transparent;
-            border: 0px;
-            -webkit-appearance: none;
-            border-radius: 0px;
-            padding: 12px 5px 12px 15px;
-            color: $light_gray;
-            height: 47px;
-            &:-webkit-autofill {
-                -webkit-box-shadow: 0 0 0px 1000px $bg inset !important;
-                -webkit-text-fill-color: #fff !important;
-            }
-        }
-    }
-    .el-form-item {
-        // border: 1px solid rgba(255, 255, 255, 0.1);
-        border: 1px solid #ffffff;
-        background: rgba(0, 0, 0, 0.1);
-        border-radius: 5px;
-        color: #454545;
-    }
-
-    .logo {
-        text-align: center;
-        line-height: 50px;
-        padding-bottom: 30px;
-        span {
-            margin-left: 8px;
-            // font-size: 18px;
+    .el-header {
+        background-color: white;
+        height: 85px !important;
+        .title {
+            bottom: 0px;
+            margin-top: 20px;
             font-weight: bold;
-            color: #ffffff;
+            font-size: 20px;
+            letter-spacing: 2px;
+            font-family: "雅黑";
+            img {
+                vertical-align: middle;
+                height: 50px;
+                width: 50px;
+            }
         }
-
-        img {
-            vertical-align: middle;
-            height: 50px;
-            width: 50px;
+        .info {
+            float: right;
+            margin-right: 20px;
+            margin-top: 38px;
+            font-size: 13px;
+            font-family: "微软雅黑";
+            color: gray;
         }
     }
-}
-</style>
-
-<style rel="stylesheet/scss" lang="scss" scoped>
-$bg: rgb(22, 86, 155);
-$dark_gray: #dee7e8; // #889aa4;
-$light_gray: #eee;
-
-.login-container {
-    position: fixed;
-    height: 100%;
-    width: 100%;
-    background-color: $bg;
-    .login-form {
-        position: absolute;
-        left: 0;
-        right: 0;
-        width: 520px;
-        padding: 35px 35px 15px 35px;
-        margin: 30px auto;
+    //     .back {
+    //     overflow: hidden;
+    //     background:rgb(35, 48, 148);
+    //     box-shadow:0 0 150px 300px rgba(12, 25, 141, 0.7) inset;
+    // }
+    .back {
+        background: #2f409a;
     }
-    .tips {
-        font-size: 14px;
-        color: #fff;
-        margin-bottom: 10px;
-        span {
-            &:first-of-type {
-                margin-right: 16px;
+    .el-main {
+        //         :after{
+        //   position:absolute;
+        //   content:'';
+        //   width:100%;
+        //   height:100%;
+        //   top:0;
+        //   left:0;
+        //   border-radius:2%;
+        //   box-shadow:0 0 30px 10px rgba(29, 25, 221, 0.7) inset;
+        // }
+        border-left: 0px;
+        box-shadow: 0 0 60px 15px #2f409a inset;
+        width: 100%; //1100px;
+        height: 550px;
+        background: url("../../../frame_src/imgs/loginback.png");
+        background-repeat: no-repeat;
+        //background-size:cover;
+        background-position: center;
+        .loginform {
+            margin-top: 5%;
+            width: 390px;
+            height: 340px;
+            background-color: rgba(252, 250, 250, 0.25);
+            //border-radius: 10px;
+            // filter: alpha(opacity=50);
+            // -moz-opacity: 0.5;
+            // -khtml-opacity: 0.5;
+            // opacity: 0.5;
+            float: right;
+            margin-right: 10%;
+            .header {
+                line-height: 40px;
+                font-size: 15px;
+                font-weight: bold;
+                font-family: "雅黑";
+                //border-radius: 10px 10px 0 0;
+                height: 40px;
+                background-color: rgba(218, 210, 210, 0.904);
+            }
+            .logo {
+                margin-left: 10%;
+            }
+            .el-form-item {
+                // border: 1px solid rgba(255, 255, 255, 0.1);
+                border: 1px solid #ffffff;
+                //background: rgba(0, 0, 0, 0.1);
+                border-radius: 5px;
+            }
+            .login-form {
+                .el-input {
+                    margin-top: 11%;
+                    margin-left: 10%;
+                    width: 80%;
+                    z-index: 2001;
+                }
+            }
+            .verification {
+                margin-left: 10%;
+                margin-top: 5px;
+                font-size: 10px;
+                color: red;
+                font-weight: bold;
+            }
+            .el-form-item__error {
+                margin-left: 10%;
+            }
+            .el-input-group__append {
+                padding: 0 15px;
+            }
+            .button {
+                float: right;
+                width: 30%;
+                margin-right: 10%;
+                margin-top: 30px;
+            }
+            .el-dropdown-link {
+                .el-input-group__append {
+                    width: 15px;
+                }
             }
         }
     }
-    .svg-container {
-        padding: 6px 5px 6px 15px;
-        color: $dark_gray;
-        vertical-align: middle;
-        width: 30px;
-        display: inline-block;
-        &_login {
-            font-size: 20px;
+
+    .el-footer {
+        background: white;
+        height: 150px;
+        .copyright {
+            margin-left: 45%;
+            //margin-bottom: 35px;
+            margin-top: 12px;
+            color: gray;
+            font-family: "微软雅黑";
+            font-size: 13px;
         }
-    }
-
-    .title {
-        font-size: 26px;
-        font-weight: 400;
-        color: $light_gray;
-        margin: 0px auto 40px auto;
-        text-align: center;
-        font-weight: bold;
-    }
-
-    .show-pwd {
-        position: absolute;
-        right: 10px;
-        top: 7px;
-        font-size: 16px;
-        color: $dark_gray;
-        cursor: pointer;
-        user-select: none;
-    }
-    .thirdparty-button {
-        position: absolute;
-        right: 35px;
-        bottom: 28px;
-    }
-    .copyright {
-        position: fixed;
-        bottom: 0px;
-        margin-left: 150px;
-        margin-bottom: 35px;
-        // color: gray;
-        color: #ffffff;
-        font-family: "微软雅黑";
-        font-size: 13px;
     }
 }
 </style>
